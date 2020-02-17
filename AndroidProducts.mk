ac5f9822bf77067e6cc8cd2f3e150436cf6380a2
enesuzun2002 <enesuzun200227@gmail.com>
2020-02-17 09:11:03 +0000

    zerofltexx: switch to COMMON_LUNCH_CHOICES
    Change-Id: Idf5a0240dcea8ef5bf3e2b05db1fec84d5398a1e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/full_zerofltexx.mk \
-    $(LOCAL_DIR)/lineage_zerofltexx.mk \
-    $(LOCAL_DIR)/aosp_zerofltexx.mk \
-    $(LOCAL_DIR)/viper_zerofltexx.mk
+    $(LOCAL_DIR)/lineage_zerofltexx.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_zerofltexx-userdebug
