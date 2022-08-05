5bb3832201efd2412bed17d41b3105f0167681be
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-08-05 17:55:35 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,7 +17,9 @@
 LOCAL_PATH := device/samsung/zerofltexx
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_PATH)/lineage_zerofltexx.mk
+    $(LOCAL_PATH)/aosp_zerofltexx.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_zerofltexx-userdebug
+    aosp_zerofltexx-userdebug \
+    aosp_zerofltexx-user \
+    aosp_zerofltexx-eng
